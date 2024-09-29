a07fd6ffbf61a961bc16a00e974a377a0435e632
sakit333 <sak528264@gmail.com>
2024-09-29 11:45:36 +0530

    new

---
+++ script.js
@@ -0,0 +1,28 @@
+const rightNav = document.querySelector(".right-nav");
+const clickElement = document.querySelector(".left-nav ul li:last-child");
+
+clickElement.addEventListener("click", () => {
+    rightNav.classList.toggle("hidden");
+});
+
+// $(document).ready(function() {
+//     $('.nav-link').click(function(event) {
+//         event.preventDefault(); // Prevent default anchor click behavior
+//         $('#content-area').load('content.html'); // Load content.html into the div
+//     });
+// });
+
+
+// function loadContent(url) {
+//     fetch(url)
+//         .then(response => {
+//             if (!response.ok) {
+//                 throw new Error('Network response was not ok');
+//             }
+//             return response.text();
+//         })
+//         .then(data => {
+//             document.getElementById('content-area').innerHTML = data;
+//         })
+//         .catch(error => console.error('There was a problem with the fetch operation:', error));
+// }
