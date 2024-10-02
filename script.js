d94cc844e60a5f5ed8d6118dab3641418b9bf009
sakit333 <sak528264@gmail.com>
2024-10-02 12:33:13 +0530

    updated with date format

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function updateTimestamp() {
     const now = new Date();
-    document.getElementById('timestamp').textContent = now.toLocaleString();
-}
+    document.getElementById('timestamp').textContent = now.toLocaleString('en-GB');}
 updateTimestamp();
 setInterval(updateTimestamp, 1000);
 
