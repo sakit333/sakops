d3092d239f2f2101302ad41d42084dbf2d8e8d4a
sakit333 <sak528264@gmail.com>
2024-11-06 13:10:03 +0530

    updated

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
-function updateTimestamp() {
-    const now = new Date();
-    document.getElementById('timestamp').textContent = now.toLocaleString('en-GB');}
-updateTimestamp();
-setInterval(updateTimestamp, 1000);
+// function updateTimestamp() {
+//     const now = new Date();
+//     document.getElementById('timestamp').textContent = now.toLocaleString('en-GB');}
+// updateTimestamp();
+// setInterval(updateTimestamp, 1000);
 
-
-
-
+function checkLogin(event) {
+    event.preventDefault();
+    var username = prompt("Enter username:");
+    var password = prompt("Enter password:");
+    if (username === "sak" && password === "sak333") {
+        window.location.href = "notes_info.html";
+    } else {
+        // window.location.href = "index.html";
+        alert("Incorrect username or password. \nPlease try again.");
+    }
+}
 
 
 
